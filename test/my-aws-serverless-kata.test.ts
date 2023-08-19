9c1a2bb95b1933aaa6cbc6ee8a5c24cab9d44113
Mario Scalas <mario.scalas@gmail.com>
2023-08-19 19:22:55 +0100

    Initial commit

---
+++ test/my-aws-serverless-kata.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyAwsServerlessKata from '../lib/my-aws-serverless-kata-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-aws-serverless-kata-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyAwsServerlessKata.MyAwsServerlessKataStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
